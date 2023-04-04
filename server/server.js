b19c4eb2acf6f25e28393d585e0e8bc6b7b4c4f3
Rahamat-E-Elahi <rahamatelahi4@gmail.com>
2023-04-04 12:31:31 -0700

    DB Connection successful

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 //Load env variables
-require("dotenv").config()
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config()
+}
 
 //Import dependencies
 const express = require("express")
+const connectToDb = require("./config/connectToDb")
 
 //Create an express app
 const app = express()
+
+//Connect to database
+connectToDb();
 
 //Routing
 app.get("/", (req,res)=>{
